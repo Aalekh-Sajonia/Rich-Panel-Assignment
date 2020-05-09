784b659aee25f1e54571884de86c6e24c26f4a20
Aalekh-Sajonia <aalekhbh@gmail.com>
2020-05-09 13:46:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,176 @@
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var passport = require("passport");
+var Strategy = require("passport-twitter").Strategy;
+var session = require('express-session');
+var Twit  = require('twit');
+var url = require('url');
+var socketIO = require('socket.io');
+var http = require('http');
+
+var T= {};
+var socketList = {};
+
+var app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+io.on('connection', (socket) => {
+  console.log(socket.id);
+  socket.on('recievedUser', (data) => {
+    console.log(data);
+    socketList[data.user] = socket.id; //just in case storing the socket ID to user ID
+    console.log(socketList);
+    console.log(T);
+    var stream = T[data.user].stream('statuses/filter', { track: '@' + data.user });
+    stream.on('tweet',  (tweet) => {
+      console.log(tweet);
+      socket.emit("newTweet", tweet);
+    })
+  })
+})
+
+// var T = new Twit({
+//     consumer_key: 'u5h3Mu4EVEOeatsJCdkAWb2ip'
+//   , consumer_secret: 'tnRS7uqqV94EiyJOisxG9lnMYXOL5DzdysuhSsY7p69I6HVKGE'
+//   , access_token: '1258283006793482240-o1mWQfQGAVmgbAKqx8t2VO2QsUVtz0'
+//   , access_token_secret: 'AoLAZco0FVlDMsgyrb4wA0MGzRnZj4g85slt4YqTxsygX'
+// });
+//
+// var stream = T.stream('statuses/filter', { track: '@aalekh_s' })
+//
+// stream.on('tweet', function (tweet) {
+//   // console.log(tweet)
+// })
+
+function reply(status, replyStatusId = null,userName) {
+  T.post('statuses/update', {
+    status,
+    replyStatusId,
+    username: `@${userName}`
+  });
+}
+
+function getTweet(id) {
+  T.get('/statuses/show/' + id, function gotData(err,data,res) {
+    // console.log(data);
+  })
+}
+
+let searchTweets = async (user) => {
+  let result = {
+    data:''
+  };
+  await T.get('search/tweets', {q: '@'+user},
+  (err,data,response) => {
+    result.data = data;
+    // console.log(result);
+    // getTweet(data.statuses[0].id_str);
+  })
+  // console.log(result)
+  return result;
+}
+
+passport.use(new Strategy({
+  consumerKey: 'u5h3Mu4EVEOeatsJCdkAWb2ip',
+  consumerSecret: 'tnRS7uqqV94EiyJOisxG9lnMYXOL5DzdysuhSsY7p69I6HVKGE',
+  callbackURL: 'http://localhost:3000'
+}, (token,tokenSecret,profile,callback) => {
+  // console.log(profile.username);
+  T[profile.username] = new Twit({
+      consumer_key: 'u5h3Mu4EVEOeatsJCdkAWb2ip'
+    , consumer_secret: 'tnRS7uqqV94EiyJOisxG9lnMYXOL5DzdysuhSsY7p69I6HVKGE'
+    , access_token: token
+    , access_token_secret: tokenSecret
+  });
+
+  console.log(T);
+  return callback(null,profile);
+}));
+
+passport.serializeUser(function(user,callback) {
+  callback(null,user);
+});
+
+passport.deserializeUser(function(obj,callback) {
+  callback(null,obj);
+});
+// app.set('views',path.join(__dirname,'views'));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session(
+  {
+    secret: 'whatever',
+    resave: true,
+    saveUninitialized: true
+  }
+))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/home',(req,res) => {
+  // res.send(`Hello World ${req.user.username}`);
+  res.render('list');
+})
+
+app.get('/twitter/login', passport.authenticate('twitter'));
+
+app.get("/login", (req,res) => {
+  res.render('login');
+})
+
+app.get('/', passport.authenticate('twitter', {
+  failureRedirect: '/twitter/login'
+}), function(req,res) {
+  res.redirect('/home');
+});
+
+app.get('/tweets', async (req,res) => {
+  // console.log(searchTweets(req.user.username));
+  if(req.user)
+  {
+    await T[req.user.username].get('search/tweets', {q: '@'+req.user.username},
+    (err,data,response) => {
+      if(err) {
+        res.send(err);
+      }
+      res.send(data);
+    })
+  }
+  res.send("error");
+  // res.send(searchTweets(req.user.username));
+})
+
+app.get('/tweet',async (req,res) => {
+  let q = url.parse(req.url,true);
+  // console.log(q.query);
+  let result = await T[req.user.username].get('/statuses/show/' + q.query.id);
+  if(result) {
+    res.send(JSON.stringify(result));
+  }
+  else {
+    res.send("error");
+  }
+})
+
+app.post('/reply', async (req,res) => {
+  // console.log(req.body);
+  T[req.user.username].post('statuses/update', {
+    in_reply_to_status_id: req.body.id,
+    status: `@${req.body.name} ` + req.body.data
+  }, (err, data, response) => {
+    if(err) {
+      res.send(err);
+    }
+    res.send(data);
+  });
+});
+
+server.listen(3000, () => console.log("App working on port 3000"));
---
+++ public/script.js
@@ -0,0 +1,217 @@
+const insertTweet = document.querySelector("#tweets");
+const selection = document.querySelector("#selection");
+const replyTweet = document.querySelector('#replyTweet');
+const replyField = document.querySelector('#replyText');
+const iconLoader = document.querySelector('#iconLoader');
+
+let socketInsetTweet = (data) => {
+  const text = `  <div id=${data.id_str} class="card mb-3" style="max-width: 450px; max-height: 450px; background-color: #74C2E1;" onclick="clicked(this.id)" >
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img style="padding: 20px;" src=${data.user.profile_image_url} class="card-img" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${data.user.name}</h5>
+          <p class="card-text">${data.text}</p>
+          <p class="card-text"><small>${data.created_at}</small></p>
+        </div>
+      </div>
+    </div>
+    </div>`;
+    insertTweet.insertAdjacentHTML('afterbegin', text);
+}
+
+let socket = io();
+socket.on('newTweet', (data) => {
+  console.log(data);
+  socketInsetTweet(data);
+});
+
+let selectedTweet = {} ;
+
+let iconLoaderFun = () => {
+  const text = `<div class="spinner-grow text-primary" role="status">
+                  <span class="sr-only">Loading...</span>
+                </div>`;
+  iconLoader.innerHTML = '';
+  iconLoader.insertAdjacentHTML("beforeend",text);
+}
+
+let iconLoaded = () => {
+  const text = `<i class="fa fa-paper-plane" aria-hidden="true"></i>`;
+  iconLoader.innerHTML = '';
+  iconLoader.insertAdjacentHTML("beforeend",text);
+}
+
+let insertSelection = (data) => {
+  selectedTweet = data;
+  let date = data.data.created_at.split(" ");
+  date = date[3];
+  const text1 = `<div class="row comment-box p-1 pt-3 pr-4">
+        <div class="col-lg-2 col-3 user-img text-center">
+          <img src=${data.data.user.profile_image_url} class="main-cmt-img">
+        </div>
+        <div class="col-lg-10 col-9 user-comment bg-light rounded pb-1">
+             <div class="row">
+                   <div class="col-lg-8 col-6 border-bottom pr-0">
+                      <p class="w-100 p-2 m-0">${data.data.text}</p>
+                   </div>
+                   <div class="col-lg-4 col-6 border-bottom">
+                      <p class="w-100 p-2 m-0"><span class="float-right"><i class="fa fa-clock-o mr-1" aria-hidden="true"></i>${date}</span></p>
+                   </div>
+             </div>
+            <div class="user-comment-desc p-1 pl-2">
+                <p class="m-0 mr-2">${data.data.user.screen_name}</p>
+            </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-11 offset-lg-1">
+        </div>
+      </div>`;
+  replyTweet.insertAdjacentHTML('beforeend',text1);
+}
+
+
+let insertSelectionSub = (data) => {
+  // selectedTweet = data;
+  let date = data.created_at.split(" ");
+  date = date[3];
+  const text1 = `<div class="row comment-box p-1 pt-3 pr-4">
+        <div class="col-lg-2 col-3 user-img text-center">
+          <img src=${data.user.profile_image_url} class="main-cmt-img">
+        </div>
+        <div class="col-lg-10 col-9 user-comment bg-light rounded pb-1">
+             <div class="row">
+                   <div class="col-lg-8 col-6 border-bottom pr-0">
+                      <p class="w-100 p-2 m-0">${data.text}</p>
+                   </div>
+                   <div class="col-lg-4 col-6 border-bottom">
+                      <p class="w-100 p-2 m-0"><span class="float-right"><i class="fa fa-clock-o mr-1" aria-hidden="true"></i>${date}</span></p>
+                   </div>
+             </div>
+            <div class="user-comment-desc p-1 pl-2">
+                <p class="m-0 mr-2">${data.user.screen_name}</p>
+            </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-11 offset-lg-1">
+        </div>
+      </div>`;
+  replyTweet.insertAdjacentHTML('beforeend',text1);
+}
+
+let test = async () => {
+  if(!selectedTweet.data || replyField.value == '') {
+    return ;
+  }
+  iconLoaderFun();
+  console.log(replyField.value,selectedTweet.data.id,selectedTweet.data.user.screen_name);
+  let data = {
+    data: replyField.value,
+    id: selectedTweet.data.id_str,
+    name: selectedTweet.data.user.screen_name
+  }
+  replyField.value = '';
+  const result = await fetch(`http://localhost:3000/reply`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  const resJSON = await result.json();
+  console.log(resJSON);
+  insertSelectionSub(resJSON);
+  iconLoaded();
+}
+
+let loaderSelection = () => {
+  const text = `<div style="text-align: center; padding: 30px;">
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>`;
+  replyTweet.insertAdjacentHTML('beforeend', text);
+}
+
+let clearLoader2 = () => {
+  replyTweet.innerHTML = '';
+}
+
+let loadTweet = async (id) => {
+  // console.log(id);
+  // selectedTweet = {} ;
+  clearLoader2();
+  loaderSelection();
+  let data = {
+    id: id
+  }
+  const result = await fetch(`http://localhost:3000/tweet?id=${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const resJSON = await result.json();
+  console.log(resJSON);
+  clearLoader2();
+  insertSelection(resJSON);
+}
+
+let clicked = (id) => {
+  console.log(id);
+  loadTweet(id);
+}
+
+let loader = () => {
+  const text = `<div style="text-align: center; padding: 30px;">
+    <div class="spinner-border" style="width: 10rem; height: 10rem;" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>`;
+  insertTweet.insertAdjacentHTML('beforeend', text);
+}
+
+let clearLoader = () => {
+  insertTweet.innerHTML = '';
+}
+
+const renderList = (items) => {
+  items.forEach(ele => {
+    const text = `  <div id=${ele.id_str} class="card mb-3" style="max-width: 450px; max-height: 450px; background-color: #74C2E1;" onclick="clicked(this.id)" >
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img style="padding: 20px;" src=${ele.user.profile_image_url} class="card-img" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${ele.user.name}</h5>
+            <p class="card-text">${ele.text}</p>
+            <p class="card-text"><small>${ele.created_at}</small></p>
+          </div>
+        </div>
+      </div>
+      </div>`;
+      insertTweet.insertAdjacentHTML('beforeend', text);
+  })
+}
+
+let load = async () => {
+  loader();
+  let result = await fetch('http://localhost:3000/tweets');
+  let res = await result.json();
+
+  console.log(res.statuses);
+  if(res) {
+    socket.emit("recievedUser", {
+      user: res.statuses[0].in_reply_to_screen_name
+    })
+  }
+  clearLoader();
+  renderList(res.statuses);
+}
+
+load();
